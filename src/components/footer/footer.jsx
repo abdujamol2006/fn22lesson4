907b1eabe7c4c2dcdaf5ad1562f468f6e26b08c4
Abdujamol <hamdamjonovabdujamol@gmail.com>
2024-05-30 23:34:05 +0500

    finished

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="@Developer0203">Abdujamol Hamdamjonov</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
